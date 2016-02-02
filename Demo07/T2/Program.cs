e3e2bf3808b660bc212230abd0c86c126ed11ae0
Kimmo Oinonen <kimmo.oinonen94@gmail.com>
2016-02-03 00:05:51 +0200

    Not finished.

--- Demo07/T2/Program.cs
+++ Demo07/T2/Program.cs
@@ -19,21 +19,44 @@ namespace T2
     {
         static void Main(string[] args)
         {
-            System.IO.StreamWriter outputFile = new System.IO.StreamWriter(@"d:\K1744\Demo07\Demo07\T1\bin\Debug\text.txt");
+            System.IO.StreamWriter outputFile = new System.IO.StreamWriter(@"c:\VisualStudioOhjelmointi\int.txt");
+            System.IO.StreamWriter outputFile2 = new System.IO.StreamWriter(@"c:\VisualStudioOhjelmointi\double.txt");
+            int num;
+            double num1;
             do
             {
                 Console.WriteLine("Give a number (enter or not a number ends): ");
                 string line = Console.ReadLine();
-                int num;
-                outputFile.WriteLine(text);
-            } while (text.Length != 0);
+                bool success = int.TryParse(line, out num);
+                if (success)
+                {
+                    outputFile.WriteLine(num);
+                }
+                else
+                {
+                    double.TryParse(line, out num1);
+                    /*if (success1)
+                    {*/
+                        outputFile2.WriteLine(num1);
+                    /*} else
+                    {
+                        Console.WriteLine("You didn't gave any number!!!");
+                    }*/
+                }
+                
+            } while (num != 0);
 
             outputFile.Close();
 
             try
             {
-                string alltext = System.IO.File.ReadAllText(@"d:\K1744\Demo07\Demo07\T1\bin\Debug\text.txt");
-                Console.WriteLine(alltext);
+                string intnums = System.IO.File.ReadAllText(@"c:\VisualStudioOhjelmointi\int.txt");
+                Console.WriteLine("Integers: ");
+                Console.WriteLine(intnums);
+
+                string dnums = System.IO.File.ReadAllText(@"c:\VisualStudioOhjelmointi\double.txt");
+                Console.WriteLine("Doubles: ");
+                Console.WriteLine(dnums);
             }
             catch (System.IO.FileNotFoundException)
             {
